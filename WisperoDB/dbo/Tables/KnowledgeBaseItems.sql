daf66020da1719ad4e1ec409b5aac7e85ad8f42c
Arturo Velarde <arturovr82@hotmail.com>
2021-01-04 08:27:13 -0300

    Adding DB definition

---
+++ WisperoDB/dbo/Tables/KnowledgeBaseItems.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[KnowledgeBaseItems] (
+    [Id]           INT            IDENTITY (1, 1) NOT NULL,
+    [Query]        NVARCHAR (MAX) NULL,
+    [Answer]       NVARCHAR (MAX) NULL,
+    [Tags]         NVARCHAR (MAX) NULL,
+    [LastUpdateOn] DATETIME       NOT NULL,
+    [RowVersion]   ROWVERSION     NOT NULL,
+    CONSTRAINT [PK_dbo.KnowledgeBaseItems] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ WisperoDB/dbo/Tables/TagItems.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[TagItems] (
+    [Id]    INT            IDENTITY (1, 1) NOT NULL,
+    [Tag]   NVARCHAR (MAX) NULL,
+    [Count] INT            NOT NULL,
+    CONSTRAINT [PK_dbo.TagItems] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
